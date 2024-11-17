fa45789ffa0c02756d12fbe69fa8e01bc8b7b44e
yash shrestha <yashcrest@gmail.com>
2024-11-17 19:59:30 +1100

    handling proper typing in userSync.ts

--- src/services/userSync.ts
+++ src/services/userSync.ts
@@ -9,33 +9,46 @@ export const syncUsers = async () => {
     const oktaUsers: OktaUser[] = await fetchOktaUsers();
 
     const slackUserEmails = new Set(
-      activeSlackUsers.map((user: any) => user.profile.email)
+      activeSlackUsers.map((user) => user.profile.email)
     );
 
-    const oktaUserEmails = new Set(
-      oktaUsers.map((user: any) => user.profile.email)
-    );
+    const oktaUserEmails = new Set(oktaUsers.map((user) => user.profile.email));
 
     //ADD new slack users to Okta
     for (const slackUser of activeSlackUsers) {
       if (!oktaUserEmails.has(slackUser.profile.email)) {
-        //logic still need to worked on
-        await onboardToOkta(
-          slackUser.profile.email,
-          slackUser.profile.first_name,
-          slackUser.profile.last_name
-        );
+        try {
+          console.log(
+            `Onboarding new user to Okta: ${slackUser.profile.email}`
+          );
+          await onboardToOkta(
+            slackUser.profile.email,
+            slackUser.profile.first_name,
+            slackUser.profile.last_name
+          );
+        } catch (error) {
+          console.error(
+            `Failed to onboard Slack user ${slackUser.profile.email} :`,
+            error
+          );
+        }
       }
     }
 
     //DELETE user from Okta (if removed from Slack)
     for (const oktaUser of oktaUsers) {
       if (!slackUserEmails.has(oktaUser.profile.email)) {
-        //logic still need to worked on
-        await removeFromOkta(oktaUser.id);
+        try {
+          console.log(`Removing user from Okta: ${oktaUser.profile.email}`);
+          await removeFromOkta(oktaUser.id);
+        } catch (error) {
+          console.error(
+            `Failed to remove Okta user ${oktaUser.profile.email}: `,
+            error
+          );
+        }
       }
     }
-
     console.log("User sync complete!");
   } catch (error) {
     console.log("Error during user sync: ", error);
